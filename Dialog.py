82c8675958f9409fcf39656586dc4ffeb3c9f173
Stephan Heijl <me@stephanheijl.com>
2013-09-21 18:51:09 +0200

    Add console interaction
    Adds console interaction to the Dialog module

--- Dialog.py
+++ Dialog.py
@@ -1,25 +1,140 @@
 import Tkinter
 import tkMessageBox
+import tkSimpleDialog
 
 class Dialog():
-	def __init__(self):				
-		root = Tkinter.Tk()
-		root.withdraw()
+	def __init__(self, v=True):
+		self.visual = v
+		if v:
+			root = Tkinter.Tk()
+			root.withdraw()
 	
-	def message(self, title, message):	
-		tkMessageBox.showinfo(title, message)
+	def drawConsoleDialog(self, type, title, message, buttons={}, fields={} ):
+		dialog = []
+		width = 50
+		textwidth = width-2
+		lines = message.split("\n")
+		print
+		print "+" + ("-"*width) + "+"
 		
+		lineN = 0
+		lineAmount = len(lines)
+		
+		while lineN < lineAmount:
+			lineAmount = len(lines)
+			
+			if len( lines[lineN] ) > textwidth:
+				line = lines[lineN]
+				del lines[lineN]
+				while len(line) > textwidth:
+					nl = " ".join( line[0:textwidth].split(" ")[0:-1] )
+					line = line[len(nl):].strip()
+					lines.insert(lineN, nl)
+					
+					lineAmount += 1
+					lineN+=1
+				
+				lines.insert(lineN, line)
+				lineAmount += 1
+				
+			else:
+				lineN+=1
+		
+		header = "%s - %s" % (type, title)
+		
+		if len(header) > width:
+			header = header[:width-5] + "..."
+			
+		centerspace = ((width-len(header))/2)
+		
+		top = "|%s %s " % ("="*centerspace, header)
+		top = top.ljust(width+1,"=")
+		top+="|"
+		print top
+		
+			
+		print "+" + ("-"*width) + "+"
+		for line in lines:
+			print "| %s |" % line.ljust(width-2, " ")
+		
+		
+		if len( fields ) > 0:
+			print "|" + (" "*width) + "|"
+			for k,v in fields.items():
+				space = textwidth-len(k)-4
+				print "| %s: [%s] |" % (k, "_"*space)
+		
+		
+		buttonsText = []
+		if len(buttons) > 0:
+			print "|" + (" "*width) + "|"
+			for k,v in buttons.items():
+				buttonsText.append( "[ %s ]" % k )
+				
+			buttonsLine = "  ".join(buttonsText)
+			centerspace = ((width-len(buttonsLine))/2)
+				
+			b = "|%s%s" % (" "*centerspace, buttonsLine)
+			b = b.ljust(width+1," ")
+			b+="|"
+			print b
+			
+		print "+" + ("-"*width) + "+"
+		
+		if len(buttons) > 0:
+			response = ""
+			while response not in buttons:
+				response = raw_input("Response: ")
+			print "'%s' was selected." % response
+			return buttons[response]
+		
+		if len(fields) > 0:
+			for f in fields:
+				fields[f] = raw_input("%s: " % f)
+				
+			return fields
+		
+									
+	def message(self, title, message):
+		if self.visual:
+			tkMessageBox.showinfo(title, message)
+		else:
+			self.drawConsoleDialog("Message",title, message)
+		
+	
 	def warning(self, title, message):	
-		tkMessageBox.showwarning(title, message)
-		
-	def error(self, title, message):	
-		tkMessageBox.showerror(title, message)
-		
-	def yesno(self, title, message):	
-		tkMessageBox.askyesno(title, message)
-		
+		if self.visual:
+			tkMessageBox.showwarning("Warning",title, message)
+		else:
+			self.drawConsoleDialog("Warning",title, message)
+	
+	def error(self, title, message):
+		if self.visual:
+			tkMessageBox.showerror("Error",title, message)
+		else:
+			self.drawConsoleDialog("Error",title, message)
+			
+	
+	def yesno(self, title, message):
+		if self.visual:
+			return tkMessageBox.askyesno(title, message)
+		else:
+			return self.drawConsoleDialog("Yes/No",title, message, buttons={'yes':True, 'no': False})
+	
 	def okcancel(self, title, message):	
-		tkMessageBox.askokcancel(title, message)
-		
-	def retrycancel(self, title, message):	
-		tkMessageBox.retrycancel(title, message)
+		if self.visual:
+			return tkMessageBox.askokcancel(title, message)
+		else:
+			return self.drawConsoleDialog("OK/Cancel",title, message, buttons={'ok':True, 'cancel': False})
+	
+	def retrycancel(self, title, message):
+		if self.visual:
+			return tkMessageBox.retrycancel(title, message)
+		else:
+			return self.drawConsoleDialog("Retry/Cancel",title, message, buttons={'retry':True, 'cancel': False})
+	
+	def prompt(self, title, message):
+		if self.visual:
+			return tkSimpleDialog.askstring(title, message)
+		else:
+			return self.drawConsoleDialog("Prompt",title, message, fields={'Response':''})['Response']
--- PortableDevSync.py
+++ PortableDevSync.py
@@ -5,6 +5,7 @@ import webbrowser
 import urllib2
 import json
 import hashlib
+import sys
 from InstallModule import InstallModule
 from Dialog import Dialog
 
@@ -13,7 +14,7 @@ class PortableDevSync():
 		try:
 			app_key, app_secret = self.__getAppKeyAndSecret()
 		except Exception as e:
-			Dialog().error("Could not contact key server", "Could not contact key server, unable to verify app. \n%s" % e)
+			Dialog(visual).error("Could not contact key server", "Could not contact key server, unable to verify app. \n%s" % e)
 			return
 		self.flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 		
@@ -23,9 +24,13 @@ class PortableDevSync():
 		return response['app_key'],response['app_secret']
 	
 	def start(self):
-		authorize_url = self.flow.start()
-		Dialog().message("Authorization needed.","You will need to authorize PortableDevSync to use your Dropbox account.\nA webpage with the authorization options will open now.")
-		webbrowser.open(authorize_url)		
+		#os.chdir(os.path.basename(os.path.abspath(__file__)))
+		
+		if not os.path.exists("settings.config"):
+			authorize_url = self.flow.start()
+			Dialog(visual).message("Authorization needed.","You will need to authorize PortableDevSync to use your Dropbox account.\nA webpage with the authorization options will open now.")
+			Dialog(visual).yesno("Open browser","Would you like PortableDevSync to open your browser?")
+			webbrowser.open(authorize_url)
 
 def getDropboxAPI():
 	try:
@@ -35,26 +40,31 @@ def getDropboxAPI():
 			im.setup()
 			return im.test()
 	except Exception as e:
-		Dialog().error("Exception", str(e))
+		Dialog(visual).error("Exception", str(e))
 		return False
 	
 if __name__ == "__main__": # AutoRun!
 	# Detect wether or not the dropbox client was installed.
-		
+	visual = True if "-text" not in sys.argv else False
+	
 	im = InstallModule(name="dropbox")
-	if im.test():
+	try:
 		import dropbox
-		#Dialog().message("Dropbox is available.", "Dropbox API was downloaded and installed succesfully.")
+		Dialog(visual).message("Dropbox is available.", "Dropbox API was downloaded and installed succesfully.")
 		dropboxAvailable = True
-	else:	
+	except ImportError:	
 		retry = True
 		while retry:
 			dropboxAvailable = getDropboxAPI()
 			if not dropboxAvailable:
 				retry = Dialog.retrycancel("Dropbox could not be installed.", "There was an error while downloading/installing the Dropbox API.")
 			else:
-				retry = False	
+				retry = False
+		
+		Dialog(visual).error("PortableDevSync could not be installed.", "PortableDevSync needs the Dropbox API to function. Installation was canceled.")
 	
-	
-	#ads = PortableDevSync()
-	#ads.start()
+	if not dropboxAvailable:
+		sys.exit()
+		
+	ads = PortableDevSync()
+	ads.start()
