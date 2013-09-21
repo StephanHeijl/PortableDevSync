c7a5392212ed3c74185ed1ff2b1c6cd0c088f133
Stephan Heijl <me@stephanheijl.com>
2013-09-21 16:24:30 +0200

    InstallModule is working
    Tested on Windows 7 (x64) with the Dropbox API plugin.
    Succesfully downloads zips, extracts them to a temporary directory,
    finds and executes setup.py.

--- InstallModule.py
+++ InstallModule.py
@@ -4,38 +4,115 @@
 import tempfile
 import urllib
 import os
+import zipfile
+import urllib2
+import shutil
+import subprocess
+import sys
 
 class InstallModule():
 	def __init__(self, name):
 		self.name = name
 		self.installed = False
+		self.tmpfile = None
+		self.tmp_extract_dir = None
 		
-	def url(url):
+	def url(self,url):
 		# Retreive a file from an url, store it in a temp directory
 		filename = url.split("/")[-1]
-		tempfile = open( os.path.join( tempfile.gettempdir() , filename ))
-		print tempfile
+		path = os.path.join( tempfile.gettempdir() , filename )
+		self.tmpfile = path
+		if not os.path.exists(path):
+			tmpfile = open( path , "wb")
+			download = urllib2.urlopen(url)
+			tmpfile.write(download.read())
+			download.close()
+			tmpfile.close()
+	
+	def unzip(self):
+		if not self.tmpfile:
+			raise Exception, "No temporary file available to unzip."
 		
+		tmp_extract_dir_name = "pds_tmp_extract_" + ".".join(os.path.basename(self.tmpfile).split(".")[:-1])
+		os.chdir( os.path.dirname(self.tmpfile) )
+		if not os.path.exists(tmp_extract_dir_name):
+			os.mkdir( tmp_extract_dir_name )
+		os.chdir( tmp_extract_dir_name )
+			
+		with zipfile.ZipFile(self.tmpfile, 'r') as archive:
+			archive.extractall()
 		
-	def pip(name):
+		self.tmp_extract_dir = os.path.join( os.path.dirname(self.tmpfile), tmp_extract_dir_name )
+	
+	def __find_setup(self, dir):
+		contents = os.listdir(dir)
+		if "setup.py" in contents:
+			return os.path.join(dir, "setup.py")
+		
+		for f in contents:
+			if os.path.isdir(f):
+				found = self.__find_setup(os.path.join(dir,f) )
+				if found:
+					return found
+					
+		return False
+		
+	
+	def setup(self, build=False, install=True):
+		if not self.tmp_extract_dir:
+			raise Exception, "No extracted files found, please use unzip() first."
+		
+		setuppath = self.__find_setup(self.tmp_extract_dir)
+		if not setuppath:
+			raise Exception, "No setup.py file found in this directory. Your downloaded file may be corrupted."
+		
+		python_executable = sys.executable
+		
+		command = " ".join([python_executable, setuppath])
+		
+		os.chdir( os.path.dirname( setuppath ) ) 
+		
+		if build:
+			build = subprocess.Popen(command + " build")
+			build.communicate()		
+		if install:		
+			install = subprocess.Popen(command + " install")
+			install.communicate()
+		
+	def pip(self,name):
 		# TODO: Implement pip code
 		pass
 		
-	def easy_install(name):
+	def easy_install(self,name):
 		# TODO: Implement easy_install code
 		pass
 		
-	def test(name=False):
+	def test(self,name=False):
 		if not name:
 			name = self.name
 			
 		try:
 			__import__(name)
-			if name == self.name():
+			if name == self.name:
 				self.installed = True
 			return True
 		except:
-			if name == self.name():
+			if name == self.name:
 				self.installed = False
 			return False
+			
+	def __enter__(self):
+		return self
+	
+	def __exit__(self, type, value, traceback):
+		if self.tmpfile:
+			os.remove(self.tmpfile)
+		if self.tmp_extract_dir:
+			try:
+				shutil.rmtree(self.tmp_extract_dir)
+			except:
+				try:
+					os.rmdir(self.tmp_extract_dir)
+				except:
+					pass
 		
--- PortableDevSync.py
+++ PortableDevSync.py
@@ -3,46 +3,58 @@
 import os
 import webbrowser
 import urllib2
-import ctypes
-import InstallModule
+import json
+import hashlib
+from InstallModule import InstallModule
 from Dialog import Dialog
 
 class PortableDevSync():
 	def __init__(self):
-		self.__app_key = 'INSERT_APP_KEY'
-		self.__app_secret = 'INSERT_APP_SECRET'
-
-		self.flow = dropbox.client.DropboxOAuth2FlowNoRedirect(self.__app_key, self.__app_secret)
+		try:
+			app_key, app_secret = self.__getAppKeyAndSecret()
+		except Exception as e:
+			Dialog().error("Could not contact key server", "Could not contact key server, unable to verify app. \n%s" % e)
+			return
+		self.flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+		
+	def __getAppKeyAndSecret(self):
+		request = urllib2.urlopen("http://cytosine.nl/~stephan/PortableDevSync/DBAppSecret.py")
+		response = json.loads(request.read())
+		return response['app_key'],response['app_secret']
 	
-	def start(self,gui=True):
+	def start(self):
 		authorize_url = self.flow.start()
 		Dialog().message("Authorization needed.","You will need to authorize PortableDevSync to use your Dropbox account.\nA webpage with the authorization options will open now.")
-		webbrowser.open(authorize_url)
-		
+		webbrowser.open(authorize_url)		
 
 def getDropboxAPI():
-	im = InstallModule()
-	im.url( "https://www.dropbox.com/static/developers/dropbox-python-sdk-1.6.zip" )
-	im.unzip()
-	im.setup()
-	im.test()
-		
+	try:
+		with InstallModule(name="dropbox") as im:
+			im.url( "https://www.dropbox.com/static/developers/dropbox-python-sdk-1.6.zip" )
+			im.unzip()
+			im.setup()
+			return im.test()
+	except Exception as e:
+		Dialog().error("Exception", str(e))
+		return False
+	
 if __name__ == "__main__": # AutoRun!
 	# Detect wether or not the dropbox client was installed.
-	try:
-		import dropbox
-		Dialog().message("Dropbox is available.", "Dropbox API was downloaded and installed succesfully.")
-		dropboxAvailable = True
 		
-	except ImportError:	
+	im = InstallModule(name="dropbox")
+	if im.test():
+		import dropbox
+		#Dialog().message("Dropbox is available.", "Dropbox API was downloaded and installed succesfully.")
+		dropboxAvailable = True
+	else:	
 		retry = True
 		while retry:
 			dropboxAvailable = getDropboxAPI()
 			if not dropboxAvailable:
 				retry = Dialog.retrycancel("Dropbox could not be installed.", "There was an error while downloading/installing the Dropbox API.")
 			else:
-				retry = False
+				retry = False	
 	
 	
-	ads = PortableDevSync()
-	ads.start(gui=True)
+	#ads = PortableDevSync()
+	#ads.start()
